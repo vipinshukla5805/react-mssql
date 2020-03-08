192c7149d5176fc276ad0b26cbedd09fc2620644
vipin5805 <vipinshukla5805@gmail.com>
2020-03-08 15:10:40 +0530

    binding

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,77 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
+import tt from './Departments.json';
+import "./App.css";
 var store = {
   headerOffset: null
 };
 
 let data = [
   {
-    id: 0,
-    name: "name 0",
-    details: "details 0",
-    state: "live"
+    Id: 1,
+    FirstName: "Pravin",
+    MiddleName: null,
+    LastName: "Panchal",
+    DOB: "1995-02-03T00:00:00.000Z",
+    Designation: "Account",
+    ReportingTo: "Ravi Kumar",
+    Salary: 35000,
+    DepID: 3
   },
   {
-    id: 1,
-    name: "name 1",
-    details: "details 1",
-    state: "live"
+    Id: 2,
+    FirstName: "Ravi",
+    MiddleName: null,
+    LastName: "Pande",
+    DOB: "1992-02-03T00:00:00.000Z",
+    Designation: "HR",
+    ReportingTo: "Gajanan",
+    Salary: 30000,
+    DepID: 1
   },
   {
-    id: 2,
-    name: "name 2",
-    details: "details 2",
-    state: "draft"
+    Id: 3,
+    FirstName: "Ishwar",
+    MiddleName: null,
+    LastName: "Bhadarge",
+    DOB: "1988-02-03T00:00:00.000Z",
+    Designation: "Software Developer",
+    ReportingTo: "Vipin",
+    Salary: 30000,
+    DepID: 2
   },
   {
-    id: 3,
-    name: "name 3",
-    details: "details 3",
-    state: "live"
-  },
-  {
-    id: 4,
-    name: "name 4",
-    details: "details 4",
-    state: "live"
-  },
-  {
-    id: 5,
-    name: "name 5",
-    details: "details 5",
-    state: "live"
-  },
-  {
-    id: 6,
-    name: "name 6",
-    details: "details 6",
-    state: "live"
-  },
-  {
-    id: 7,
-    name: "name 7",
-    details: "details 7",
-    state: "live"
-  },
-  {
-    id: 8,
-    name: "name 8",
-    details: "details 8",
-    state: "live"
-  },
-  {
-    id: 9,
-    name: "name 9",
-    details: "details 9",
-    state: "live"
-  },
-  {
-    id: 10,
-    name: "name 10",
-    details: "details 10",
-    state: "live"
-  }                
+    Id: 4,
+    FirstName: "Vipin",
+    MiddleName: null,
+    LastName: "Shukla",
+    DOB: "1985-02-09T00:00:00.000Z",
+    Designation: "Senior Software Developer",
+    ReportingTo: "Rahul",
+    Salary: 60000,
+    DepID: 2
+  }
 ];
 
 let cols = [
@@ -81,7 +61,7 @@ let cols = [
   },
   {
     icon: "",
-    label: "Order Number"
+    label: "Id"
   },
   {
     icon: "",
@@ -89,217 +69,158 @@ let cols = [
   },
   {
     icon: "",
-    label: "Details"
+    label: "Account"
   },
   {
     icon: "",
-    label: "State"
-  }  
-]
-
+    label: "Department"
+  }
+];
 
 class RowItem extends React.Component {
-  
   constructor() {
     super();
-    
+
     this.state = {
-      open: false
-    }
-  }  
-  
-  toggleRow(e) {
-    console.log('toggleRow');
-    
-    this.setState({open: !this.state.open});
+      open: false,
+      dep: []
+    };
   }
-  
-  
-  render(){
+
+  toggleRow(e) {
     
-    let classes = '';
+    let deps = this.getData();
+    this.setState({ open: !this.state.open , dep: deps});
+  }
+
+   getData = ()=>{
+    // let res = await axios.get(tt);
+    debugger
+    return tt;
+  }
+
+  render() {
+    let classes = "";
     if (this.state.open) {
-      classes = 'open';
+      classes = "open";
     }
-    
+
     return (
       <li onClick={this.toggleRow.bind(this)} className={classes}>
         <div className="heading">
-          <div className="col"><input type="checkbox" checked={this.props.selected}  /></div>
-          <div className="col">{this.props.id}</div>
-          <div className="col">{this.props.name}</div>
-          <div className="col">{this.props.details}</div>             <div className="col">{this.props.state}</div>     
+          <div className="col">
+            <input type="checkbox" checked={this.props.selected} />
+          </div>
+          <div className="col">{this.props.Id}</div>
+          <div className="col">{this.props.FirstName}</div>
+          <div className="col">{this.props.Designation}</div>
+          <div className="col">{this.props.DepID}</div>
         </div>
-        <RowContent open={this.state.open}/>
+        <RowContent open={this.state.open} deps={this.state.dep} />
         {this.props.children}
       </li>
-    )
+    );
   }
-  
-};
-
+}
 
 class RowContent extends React.Component {
-  
   clicker() {
-    
+    console.log('hi......')
   }
-  
-  render(){
-    
-    let jsxhtml = (<div className="content" onClick={this.clicker.bind(this)}>
-        
-        <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>
 
-      </div>);
-    
+  render() {
+    let jsxhtml;
     if (this.props.open) {
-      jsxhtml = (<div className="content open" onClick={this.clicker.bind(this)}>
-        <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>
-
-      </div>);
-    }
-    
-    return (
-      <div>
-      {jsxhtml}
+      jsxhtml = (
+        <div className="content open" onClick={this.clicker.bind(this)}>
+          <table>
+            <tr>
+              <th>Id</th>
+              <th>Dep Name</th>
+            </tr>
+            {
+              this.props.deps.map(item => <tr>
+                <td>{item.Id}</td>
+              <td>{item.Name}</td>
+              </tr>)
+            }
+          </table>
         </div>
-    )
-  }
-  
-};
+      );
+    }
 
+    return <div>{jsxhtml}</div>;
+  }
+}
 
 class Table extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
       headerOffset: null,
       headerFixed: true
-    }
+    };
   }
   componentDidMount() {
-  //   window.addEventListener('scroll', this.handleScroll.bind(this));
+    //   window.addEventListener('scroll', this.handleScroll.bind(this));
     // THIS SEEMS THE ONLY PLACE WE CAN PICK UP THE REF FOR THE HEADER
-    console.log('reactdom: ', ReactDOM.findDOMNode(this.refs.header));
-    
-    store.headerOffset = ReactDOM.findDOMNode(this.refs.header).getBoundingClientRect().top;  
-    
-    
-    console.log('store:', store.headerOffset);
-    
-    
+    console.log("reactdom: ", ReactDOM.findDOMNode(this.refs.header));
+
+    store.headerOffset = ReactDOM.findDOMNode(
+      this.refs.header
+    ).getBoundingClientRect().top;
+
+    console.log("store:", store.headerOffset);
+
     // this.setState({headerOffset:ReactDOM.findDOMNode(this.refs.header)});
   }
 
-  selectAll = (event)=>{
-    this.setState({allSelected: event.target.checked})
-    this.props.data.map(item=>{
-      return item.selected = event.target.checked;
-    })
-  }
-  
-  render(){
-    
+  selectAll = event => {
+    this.setState({ allSelected: event.target.checked });
+    this.props.data.map(item => {
+      return (item.selected = event.target.checked);
+    });
+  };
+
+  render() {
     let columns = this.props.columns.map((item, inx) => {
-      if(inx === 0){
-        return( <div className="hcol" style={{textAlign:'center'}}>Select All <input type="checkbox" checked={this.state.allSelected} onClick={this.selectAll} /></div>)
+      if (inx === 0) {
+        return (
+          <div className="hcol" style={{ textAlign: "center" }}>
+            Select All{" "}
+            <input
+              type="checkbox"
+              checked={this.state.allSelected}
+              onClick={this.selectAll}
+            />
+          </div>
+        );
       }
-      return (<HeaderColumn label={item.label}/>);
-    });    
-    
+      return <HeaderColumn label={item.label} />;
+    });
+
     //go through the rows
     let rows = this.props.data.map((item, inx) => {
-      return (<RowItem {...item}></RowItem>);
+      return <RowItem {...item}></RowItem>;
     });
-    
-    let classes = 'header'; 
+
+    let classes = "header";
     if (this.props.headerFixed) {
-      classes = 'header fixed';
+      classes = "header fixed";
     }
-    
-   return (<div className="table">
-          {this.props.children} 
-       <div className={classes} ref="header">
-       {columns}
-       <div className="shadow"></div></div>
-          <ul>{rows}</ul>
-         </div>); 
+
+    return (
+      <div className="table">
+        {this.props.children}
+        <div className={classes} ref="header">
+          {columns}
+          <div className="shadow"></div>
+        </div>
+        <ul>{rows}</ul>
+      </div>
+    );
   }
-  
 }
 
 class HeaderColumn extends React.Component {
@@ -307,36 +228,33 @@ class HeaderColumn extends React.Component {
   constructor() {
     super();
   }
-  
-  render(){
-   return (<div className="hcol">{this.props.label}</div>); 
+
+  render() {
+    return <div className="hcol">{this.props.label}</div>;
   }
-  
 }
 
-
-class App extends React.Component{
+class App extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
       tableHeader: null,
       tableHeaderFixed: false
-    }
-    
+    };
   }
-  
+
   handleScroll(e) {
     // console.log(e);
     let scrollTop = e.srcElement.body.scrollTop;
-    
+
     //HOW DO WE GET THE REFS HERE FOR THE ITEM OFFSET?
-    
-        //itemTranslate = Math.min(0, scrollTop/3 - 60);
-    console.log('app scroll...', scrollTop, store.headerOffset);
+
+    //itemTranslate = Math.min(0, scrollTop/3 - 60);
+    console.log("app scroll...", scrollTop, store.headerOffset);
     // console.log('reactdom: ', ReactDOM.findDOMNode(this.refs.header), this.refs);
     // console.log(ReactDOM.findDOMNode(this.refs.header));
-    
+
     if (scrollTop >= store.headerOffset) {
       this.setState({
         tableHeaderFixed: true
@@ -347,23 +265,24 @@ class App extends React.Component{
       });
     }
   }
-  
+
   componentDidMount() {
-    console.log('app did mount');
-    window.addEventListener('scroll', this.handleScroll.bind(this));
-  }  
-  
-  
-  render(){
-     
-    
+    console.log("app did mount");
+    window.addEventListener("scroll", this.handleScroll.bind(this));
+  }
+
+  render() {
     return (
       <div className="container">
-        <Table data={data} columns={cols} headerFixed={this.state.tableHeaderFixed} scrollFn=''/>
+        <Table
+          data={data}
+          columns={cols}
+          headerFixed={this.state.tableHeaderFixed}
+          scrollFn=""
+        />
       </div>
-    )
+    );
   }
-  
-};
+}
 
 export default App;
